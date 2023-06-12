bf67b9cb3271f803d5085c2c05ca946ff8e7ed26
Erman Dogan <extern.dogan_erman@allianz.de>
2023-06-12 14:06:50 +0200

    create todo form

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {todos} from "../../todos";
+import {Todo} from "../../models/Todo";
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+  name!: string
+  date!: string
+
+
+
+  sendForm(){
+    const todo: Todo = {
+      name: this.name,
+      date: this.date,
+      done: false
+    }
+    todos.push(todo)
+    // this.name = ''
+    // this.date = ''
+  }
+}
